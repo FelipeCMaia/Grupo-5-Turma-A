3306edbb529d242691e533f5c488b49048fdf113
Felipe Maia <felipe.cmaia@hotmail.com>
2022-06-07 18:00:03 -0300

    Ultimos ajustes

--- Genesis/src/Main/App.java
+++ Genesis/src/Main/App.java
@@ -14,15 +14,13 @@ public class App {
 
 		String menu;
 
-		String nome, nrobo;
-
 		boolean errado = true;
-		
+
 		// Do para repetir o jogo ate a opcao de sair
 		do {
 
 			System.out.println(" -- MENU -- ");
-			// 
+			//
 			System.out.println("<1> Instrucoes");
 			//
 			System.out.println("<2> Jogar");
@@ -31,7 +29,7 @@ public class App {
 			//
 			System.out.println("<4> Sair");
 
-			menu = entrada.next();			
+			menu = entrada.next();
 
 			// Switch que verifica qual a opcao selecionada pelo usuario
 			switch (menu) {
@@ -51,7 +49,7 @@ public class App {
 
 							+ "- Medio - Perguntas medias, perdendo 1 vida; \n"
 
-							+ "- Dificil - Perguntas dificeis, perdendo 2 vidas.");					
+							+ "- Dificil - Perguntas dificeis, perdendo 2 vidas.");
 					break;
 
 				case "2":
@@ -87,6 +85,8 @@ public class App {
 		Utilitarios.LimparTela();
 
 		// Declaracao de variaveis
+		String nome;
+		int dificuldade;
 		int vidas = 5;
 		Scanner reader = new Scanner(System.in);
 		String resposta = null;
@@ -94,8 +94,29 @@ public class App {
 		Pergunta pergunta = null;
 		ArrayList<Pergunta> perguntasFase1 = Utilitarios.ObterPerguntasFase(1);
 		ArrayList<Pergunta> perguntasFase2 = Utilitarios.ObterPerguntasFase(2);
+		boolean valido = false;
 
-		//#region Exibicao da Historia
+		System.out.println("Digite o seu nome: ");
+		nome = reader.nextLine();
+
+		do {
+			System.out.println("Selecione a dificuldade");
+			System.out.println("1 - Fácil");
+			System.out.println("2 - Médio");
+			System.out.println("3 - Difícil");
+			dificuldade = reader.nextInt();
+
+			if (dificuldade < 1 || dificuldade > 3) {
+				System.out.println("Digite um valor valido");
+			} else {
+				valido = true;
+			}
+
+		} while (!valido);
+
+		Utilitarios.LimparTela();
+
+		// #region Exibicao da Historia
 
 		Utilitarios.ExibirTexto("Seu recomeço se inicia em 2100, em um mundo devastado e controlado pelas \n"
 				+ "maquinas que utilizam os seres humanos que restaram como fonte de energia e os rejeitados \n"
@@ -142,44 +163,45 @@ public class App {
 
 		Utilitarios.LimparEAguardar();
 
-		//#endregion
+		// #endregion
 
 		System.out.println("\n \n");
 
 		// For que realiza a primeira fase de perguntas
-		for (int i = 0; i < 5; i++) {			
+		for (int i = 0; i < 5; i++) {
 			pergunta = perguntasFase1.get(i);
 
 			System.out.println((i + 1) + " - " + pergunta.pergunta);
 
 			// For para exibir as alternativas
-			for(int j = 0; j < pergunta.alternativas.size(); j++) {
+			for (int j = 0; j < pergunta.alternativas.size(); j++) {
 				alt = pergunta.alternativas.get(j);
 
-				System.out.println(alt.letra + ") " + alt.texto);				
+				System.out.println(alt.letra + ") " + alt.texto);
 			}
 
-			resposta = reader.next();						
+			resposta = reader.next();
 
 			Utilitarios.LimparTela();
 
 			// If que verifica se a resposta digitada foi correta
 			// Se nao diminui as vidas restantes
-			if(pergunta.RespostaCorreta(resposta.toUpperCase())) {
-				System.out.println("Parabens!!! Resposta Correta!! \n");								
+			if (pergunta.RespostaCorreta(resposta.toUpperCase())) {
+				System.out.println("Parabens!!! Resposta Correta!! \n");
 			} else {
 				System.out.println("Ops!! Resposta Errada \n");
-				vidas--;
+				vidas = vidas - dificuldade;
 
 				// Valida se ainda possui vidas para continuar no jogo para
 				// Se nao encerra o jogo
-				if(!PossuiVidas(vidas)) {
+				if (!PossuiVidas(vidas)) {
 					Utilitarios.LimparTela();
-	
-					Utilitarios.ExibirTexto("Não, seu Robô foi destruído, corra, fuja, se esconda. Você é nossa única esperança tente novamente, mas não desista. \n \n");					
+
+					Utilitarios.ExibirTexto(
+							"Não, seu Robô foi destruído, corra, fuja, se esconda. Você é nossa única esperança tente novamente, mas não desista. \n \n");
 
 					// System.in.read();
-	
+
 					return;
 				}
 
@@ -188,52 +210,53 @@ public class App {
 		}
 
 		System.out.println('-');
-		
+
 		Utilitarios.ExibirTexto("Após esses desafios, você finalmente consegue montar seu próprio Robô, \n"
-		+ "uma máquina mortífera que irá te ajudar na sua vingança assim por dizer. Porém teste lhe aguardam \n"
-		+ "para alcançar o núcleo de todo esse caos no planeta terra, vá divergente use sua inteligência e enfrente seus inimigos. \n");
-	
+				+ "uma máquina mortífera que irá te ajudar na sua vingança assim por dizer. Porém teste lhe aguardam \n"
+				+ "para alcançar o núcleo de todo esse caos no planeta terra, vá divergente use sua inteligência e enfrente seus inimigos. \n");
+
 		Utilitarios.LimparEAguardar();
 
 		System.out.println(" \n Fase 2 \n \n");
 
-		Utilitarios.ExibirTexto("Agora com seu sua máquina pronta você parte em direção ao centro de controle do inimigo \n " 
-		+ "onde suas forças estão concentradas e onde você deverá enfrentrar seu primeiro inimigo na busca de seu objetivo");
-		
+		Utilitarios
+				.ExibirTexto("Agora com seu sua máquina pronta você parte em direção ao centro de controle do inimigo \n "
+						+ "onde suas forças estão concentradas e onde você deverá enfrentrar seu primeiro inimigo na busca de seu objetivo");
+
 		// For que realiza a segunda fase de perguntas
-		for (int i=1; i <= 3; i++) {			
+		for (int i = 1; i <= 3; i++) {
 			pergunta = perguntasFase2.get(i);
 
 			System.out.println(i + " - " + pergunta.pergunta);
 
 			// For para exibir as alternativas
-			for(int j = 0; j < pergunta.alternativas.size(); j++) {
+			for (int j = 0; j < pergunta.alternativas.size(); j++) {
 				alt = pergunta.alternativas.get(j);
 
-				System.out.println(alt.letra + ") " + alt.texto);				
+				System.out.println(alt.letra + ") " + alt.texto);
 			}
 
-			resposta = reader.next();						
+			resposta = reader.next();
 
 			Utilitarios.LimparTela();
 
 			// If que verifica se a resposta digitada foi correta
 			// Se nao diminui as vidas restantes
-			if(pergunta.RespostaCorreta(resposta.toUpperCase())) {
+			if (pergunta.RespostaCorreta(resposta.toUpperCase())) {
 				// Funcao que exibe a mensagem de vitoria de acordo com o inimigio enfrentado
 				ExibirTextoFases(i, true);
 			} else {
 				System.out.println("Ops!! Resposta Errada \n");
-				vidas--;
+				vidas = vidas - dificuldade;
 
 				// Regressa o i para repetir a mesma pergunta
 				i--;
 
 				// Valida se ainda possui vidas para continuar no jogo para
 				// Se nao encerra o jogo
-				if(!PossuiVidas(vidas)) {
+				if (!PossuiVidas(vidas)) {
 					Utilitarios.LimparTela();
-	
+
 					// Funcao que exibe a mensagem de vitoria de acordo com o inimigio enfrentado
 					ExibirTextoFases(i, false);
 
@@ -250,46 +273,49 @@ public class App {
 	}
 
 	public static void ExibirTextoFases(int fase, boolean correto) throws Exception {
-		//Switch que determina qual a mensagem que será exibida de acorod com a fase
+		// Switch que determina qual a mensagem que será exibida de acorod com a fase
 		switch (fase) {
-			case 1 :
-			if(correto) {
-				Utilitarios.ExibirTexto("Bravo Divergente, falta pouco continue assim e logo alcançara seu objetivo. \n"
-				+ "Seu robô está cada vez mais forte enfrente mais robôs e pegue suas peças.");
+			case 1:
+				if (correto) {
+					Utilitarios.ExibirTexto("Bravo Divergente, falta pouco continue assim e logo alcançara seu objetivo. \n"
+							+ "Seu robô está cada vez mais forte enfrente mais robôs e pegue suas peças.");
 
- 			} else {
-				Utilitarios.ExibirTexto("Não, seu Robô foi destruído, corra, fuja, se esconda. \n"
-				+ "Você é nossa única esperança tente novamente, mas não desista.");
-			}
-			break;
+				} else {
+					Utilitarios.ExibirTexto("Não, seu Robô foi destruído, corra, fuja, se esconda. \n"
+							+ "Você é nossa única esperança tente novamente, mas não desista.");
+				}
+				break;
 
 			case 2:
-			if(correto) {
-				Utilitarios.ExibirTexto("Após uma árdua batalha, finalmente o guardião cai e todas suas peças começam a entrar em curto circuito, afirmando sua derrota. \n"
-				+ "Seu próximo desafio o aguarda e está muito próximo, continue divergente.");
+				if (correto) {
+					Utilitarios.ExibirTexto(
+							"Após uma árdua batalha, finalmente o guardião cai e todas suas peças começam a entrar em curto circuito, afirmando sua derrota. \n"
+									+ "Seu próximo desafio o aguarda e está muito próximo, continue divergente.");
 
- 			} else {
-				Utilitarios.ExibirTexto("Seu Robô foi destruído, mesmo que você corra não irá conseguir fugir, \n"
-				+ "não tem como fugir do guardião, não tem como se esconder dele. E nossa última esperança está perdida... \n"
-				+ "Ou será que não (Tente novamente) ...");
-			}
-			break;
+				} else {
+					Utilitarios.ExibirTexto("Seu Robô foi destruído, mesmo que você corra não irá conseguir fugir, \n"
+							+ "não tem como fugir do guardião, não tem como se esconder dele. E nossa última esperança está perdida... \n"
+							+ "Ou será que não (Tente novamente) ...");
+				}
+				break;
 
 			case 3:
-			if(correto) {
-				Utilitarios.ExibirTexto("Você cai de joelhos, perante o grande robô a sua frente, depois da longínqua batalha você ver um único robô em pé, \n"
-				+ "esse robô é a salvação de toda humanidade. Você começa a gritar, um grito que ecoa em toda torre, o alivio se estende em todo seu corpo a libertação está próxima, \n"
-				+ "finalmente você pega o chip da máquina mãe e usa para liberta todos os seres humanos que estavam na torre. Finalmente, todos estão livres, você que está no controle \n"
-				+ "central e passa um aviso para todos os habitantes que estavam nas fazendas através dos robôs. “Vocês estão livres, irei desligar todos os robôs, essa é nossa vitória.” \n"
-				+ "E então todos os robôs começam a cair no chão e agora parece que tudo vai dá certo, parece... \n \n \n"
-				+ "FIM... (será) ...");
+				if (correto) {
+					Utilitarios.ExibirTexto(
+							"Você cai de joelhos, perante o grande robô a sua frente, depois da longínqua batalha você ver um único robô em pé, \n"
+									+ "esse robô é a salvação de toda humanidade. Você começa a gritar, um grito que ecoa em toda torre, o alivio se estende em todo seu corpo a libertação está próxima, \n"
+									+ "finalmente você pega o chip da máquina mãe e usa para liberta todos os seres humanos que estavam na torre. Finalmente, todos estão livres, você que está no controle \n"
+									+ "central e passa um aviso para todos os habitantes que estavam nas fazendas através dos robôs. “Vocês estão livres, irei desligar todos os robôs, essa é nossa vitória.” \n"
+									+ "E então todos os robôs começam a cair no chão e agora parece que tudo vai dá certo, parece... \n \n \n"
+									+ "FIM... (será) ...");
 
- 			} else {
-				Utilitarios.ExibirTexto("Você cai de joelhos, perante o grande robô a sua frente, a condenação para humanidade está próxima, "
-				+ "a esperança está morta. Não temos mais salvação, a máquina mãe é muito forte não tem como vença-la, você ver o grande robô "
-				+ "trucidar o seu robô em meros pedaços e você sente que o seu fim será o mesmo...");
-			}						
-			break;			
+				} else {
+					Utilitarios.ExibirTexto(
+							"Você cai de joelhos, perante o grande robô a sua frente, a condenação para humanidade está próxima, "
+									+ "a esperança está morta. Não temos mais salvação, a máquina mãe é muito forte não tem como vença-la, você ver o grande robô "
+									+ "trucidar o seu robô em meros pedaços e você sente que o seu fim será o mesmo...");
+				}
+				break;
 		}
 
 		Utilitarios.LimparEAguardar();
